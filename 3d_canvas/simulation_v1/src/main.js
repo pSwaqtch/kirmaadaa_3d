7aa5d09d58f8bc7ef3be80615ec68b4cda1dfe87
krupal <krupalvirani@gmail.com>
2024-12-28 16:31:57 +0530

    modularised code and prepared canvas to have a frame div

--- 3d_canvas/simulation_v1/src/main.js
+++ 3d_canvas/simulation_v1/src/main.js
@@ -1,24 +1,20 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-setupCounter(document.querySelector('#counter'))
+import { init, animate, onWindowResize } from "./scene";
+import { loadSliceDataFromJSON } from "./utils";
+
+const cubeData = await loadSliceDataFromJSON("framedata.json");
+
+window.addEventListener("resize", onWindowResize);
+
+async function main() {
+  init(cubeData);
+  animate();
+  // export both the cubeData and cubeDataJSON to a csv
+  // exportToCSV(cubeData, "cubeData.csv");
+  // exportToCSV(cubeData, "cubeDataJSON.csv");
+}
+main().catch(console.error);
